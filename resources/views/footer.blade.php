90cafec298708efe2e2db8c7dbf3abe89c04d02f
Rahmawati Setyorini <setyorinirahma.rw@gmail.com>
2023-07-31 10:17:08 +0700

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,29 @@
+<p>
+    <!--awal FOOTER-->
+    <div class="col-md-12">
+        <div class="row" style="background:rgb(240, 248, 254)">
+            <div class="col-md-9">
+                <h3>Visi</h3>
+                <p>Menunjang Tridarma Perguruan Tinggi dengan fungsinya sebagai sumber informasi bagi 
+                    pelaksanaan proses belajar dan mengajar, penelitian dan pengabdian pada masyarakat.
+                <p>
+                <h3>Misi</h3>
+                <ul>
+                    <li>Memberikan layanan prima dalam mengakses dan menyediakan informasi/bahan pustaka yang diperlukan</li>
+                    <li>Mengembangkan ragam layanan yang mendukung pengajaran, penelitian dan pengabdian pada masyarakat</li>
+                    <li>Mengembangkan koleksi bahan pustaka dalam bentuk digital dan virtual</li>
+                    <li>Memberikan rasa aman, nyaman, tentram dan rekreatif bagi pemustaka</li>
+                    <li>Meningkatkan kualitas sumber daya pelayanan perpustakaan menjadi relevan dan profesional dalam bidangnya</li>
+                </ul>                     
+            </div>
+            <div class="col-md-3">
+                Copyright &copy; 2022. All right reserve
+                <p>
+                Developed by Rahma</div>
+        </div>
+    </div>
+    <!--akhir FOOTER-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+       
---
+++ resources/views/view_home.blade.php
@@ -0,0 +1,34 @@
+@extends('index')
+@section('title', 'Home')
+
+@section('isihalaman')
+    <h3>Profil perpustakaan Universitas</h3>
+    <p>
+    <p>
+    <h4>Perpustakaan Universitas</h4>
+        Sebagai tempat menimba ilmu pengetahuan. Universitas menyediakan Perpustakaan yang dilengkapi dengan 
+        beragam bahan pustaka yang terdiri dari buku literatur baik dalam bahasa Indonesia maupun dalam bahasa Inggris, 
+        majalah, jurnal ilmiah serta buku ilmu pengetahuan lainnya. Fasilitas Perpustakaan Universitas telah 
+        digunakan oleh mahasiswa, dosen, karyawan dan alumni Universitas. Sesuai dengan kemajuan teknologi komunikasi 
+        telah dikembangkan perpustakaan audio visual di Universitas. Perpustakaan ini dilengkapi dengan peralatan video 
+        dan TV , juga perangkat komputer untuk keperluan belajar mandiri.Koleksi perpustakaan bersifat satu arah dan dua arah 
+        (multi media interaktif) Apabila seorang mahasiswa kesulitan dalam salah satu mata kuliah, mahasiswa dapat mengunjungi 
+        perpustakaan audio visual kemudian mencari koleksi yang diinginkan, kemudiana belajar secara mandiri. Dengan perpustakaan 
+        audio visual diharapkan keterbatasan ruang dan waktu dapat dihilangkan. seorang mahasiswa sastra Inggris, dapat mempelajari 
+        bagaimana kebudayaan inggris dan karya sastra klasik inggris melalui video, atau melalui audio. Untuk memenuhi koleksi video 
+        dan multi media interaktif, perpustakaan audio visual dilengkapi dengan studio produksi. distudio ini diproduksi video dan multi 
+        media interaktif. Direncanakan akan di produksi video/vcd dan multimedia interaktif untuk semua mata kuliah yang diberikan di universitas. 
+        Para mahasiswa, dosen dan karyawan serta pihak lain yang berkepentingan dapat memanfaatkan fasilitas tersebut guna mendukung pelaksanaan kegiatan belajar mengajar. 
+
+    <p>
+    <h4>Sejarah & Alamat Perpustakaan Universitas</h4>
+        Sejarah 1987 Berdiri Perpustakaan STKG di kampus Depok 1992 Berdiri Perpustakaan STIE di kampus Kelapa Dua 1996 Bertambah 4 buah Perpustakaan 
+        fakultas yaitu: Perpustakaan fakultas Teknik Perencanaan, Teknik Industri, Psikologi dan Sastra 2000 Secara keseluruhan disebut sebagai Perpustakaan 
+        Universitas Lokasi Operasional Kampus D Jalan Margonda Raya No. 100 Depok Telepon : 78881112 - ext. 301 Gedung III Lt. 1 Perpustakaan Fakultas Ilmu Komputer, 
+        Fakultas Teknologi Industri, dan Fakultas Teknik Perencanaan dan Sipil
+    <p>
+    <h4>Perpustakaan Menurut UU NO 43. THN. 2007</h4>
+    Perpustakaan adalah institusi yang mengumpulkan pengetahuan tercetak dan terekam, mengelolanya dengan cara khusus guna
+    memenuhi kebutuhan intelektualitas para penggunanya melalui berbagai cara interaksi pengetahuan.
+
+@endsection
